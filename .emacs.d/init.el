878e1ea5e82103097fd49b63b37bc3e9a8a237f7
furusho <furusityofu@gmail.com>
2018-07-16 18:54:44 +0900

    elpyとorg-beamerの設定を変更

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -150,7 +150,7 @@
 (require 'ox-bibtex)
 
 (setq org-latex-default-class "bxjsarticle")
-(setq org-latex-pdf-process '("latexmk  -gg -pdfdvi %f"))
+(setq org-latex-pdf-process '("latexmk  %f"))
 ;(setq org-latex-pdf-process '("latexmk -e '$lualatex=q/lualatex %S/' -e '$bibtex=q/upbibtex %B/' -e '$biber=q/biber --bblencoding=utf8 -u -U --output_safechars %B/' -e '$makeindex=q/upmendex -o %D %S/' -norc -gg -pdflua %f"))
 ;(setq org-export-in-background t)
 (setq org-file-apps
@@ -489,7 +489,12 @@
 (use-package elpy
   :ensure t
   :config
-  (elpy-enable)  )
+  (elpy-enable)
+  (setq python-shell-interpreter "jupyter"
+        python-shell-interpreter-args "console --simple-prompt"
+        python-shell-prompt-detect-failure-warning nil)
+  (add-to-list 'python-shell-completion-native-disabled-interpreters
+               "jupyter"))
 
 
 (use-package spaceline :ensure t
