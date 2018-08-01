7560618f080516177243fc7dbfb85b8ab895da0e
furusho <furusityofu@gmail.com>
2018-08-01 09:53:46 +0900

    閉括弧が欠けていたのを修正

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -709,4 +709,4 @@
       (add-to-list 'company-backends 'company-irony)
       (add-hook 'c++-mode-hook 'irony-mode)
       (add-hook 'c-mode-hook 'irony-mode)
-      (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))  )
+      (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))  ))
