092ca90053c885bbf8733279f9847cddad764a85
FURUSHO Hiroki <furusityofu@gmail.com>
2019-07-10 11:28:58 +0900

    companyの設定をinit.elに移動

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -563,6 +563,41 @@
   :ensure t)
 (setq-default tab-width 4 indent-tabs-mode nil)
 
+(use-package company
+  :ensure t
+  :bind (
+         ;; :map company-mode-map
+         ;;      ((kbd "TAB") . 'company-indent-or-complete-common)
+         :map company-active-map
+              ("C-n"   . 'company-select-next)
+              ("C-p"   . 'company-select-previous)
+              ("C-s"   . 'company-filter-candidates)
+              ("C-i"   . 'company-complete-selection)
+         :map company-search-map
+              ("C-n"   . 'company-select-next)
+              ("C-p"   . 'company-select-previous))
+  :config
+  ;; (global-company-mode 1)
+  (define-key company-mode-map (kbd "C-M-i") 'company-complete)
+  (setq company-idle-delay 0) ; 遅延なしにすぐ表示
+  (setq company-minimum-prefix-length 2)
+  (setq company-selection-wrap-around t)
+  (defvar company-mode/enable-yas t
+    "Enable yasnippet for all backends.")
+
+  (defun company-mode/backend-with-yas (backend)
+	(if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+		backend
+      (append (if (consp backend) backend (list backend))
+              '(:with company-yasnippet))))
+  ;; (push '(company-semantic :with company-yasnippet) company-backends)
+  ;(custom-set-variables '(company-idle-delay nil))
+
+  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+  (push 'company-lsp company-backends)
+  (setq company-lsp-enable-snippet t)
+  (setq company-lsp-enable-recompletion t))
+
 
 ;(cua-mode t) ; cua-modeをオン
 ;(setq cua-enable-cua-keys nil)
