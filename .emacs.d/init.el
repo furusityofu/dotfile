cfeab7f3c89641c855c471a3f99a6d910626e90e
furusho <furusityofu@gmail.com>
2019-09-11 10:51:11 +0900

    ddskkの設定を分けた

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -134,6 +134,7 @@
 (load "yatex-init")
 (load "mu4e-init")
 (load "magit-init")
+(load "skk-init")
 (define-key global-map (kbd "C-c t l") 'toggle-truncate-lines)
 (add-hook 'dired-load-hook
           (load "dired-x")
@@ -218,41 +219,6 @@
   (load-theme 'spacemacs-dark t))
 
 
-(use-package ddskk
-  :ensure t
-  :bind (("C-x C-j" . skk-mode))
-  :init
-  (setq skk-large-jisyo "~/.emacs.d/skk-get-jisyo/SKK-JISYO.L")
-  (setq skk-search-katakana t)
-  (setq skk-use-act t)
-  (setq skk-henkan-show-candidates-keys '(?a ?o ?e ?u ?h ?t ?n ?s))
-  (setq-default skk-kutouten-type 'en)
-  (setq skk-dcomp-activate t)
-  (setq skk-rom-kana-rule-list
-        '(("tni" nil ("ティ" . "てぃ"))
-          ("dni" nil ("ディ" . "でぃ"))))
-  ;; isearch
-  (add-hook 'isearch-mode-hook 'skk-isearch-mode-setup) ; isearch で skk のセットアップ
-  (add-hook 'isearch-mode-end-hook 'skk-isearch-mode-cleanup) ; isearch で skk のクリーンアップ
-  (setq skk-isearch-start-mode 'latin); isearch で skk の初期状態
-
-  :config
-  (setq skk-egg-like-newline t);;non-nilにするとEnterでの確定時に改行しない
-  ;; ▼モードで BS を押したときには確定しないで前候補を表示する
-  (setq skk-delete-implies-kakutei nil)
-  (setq skk-dcomp-activate t))
-
-
-(use-package skk-study
-  :disabled nil
-  :after ddskk)
-
-;;wget https://raw.githubusercontent.com/skk-dev/ddskk/master/bayesian/skk-bayesian.el
-
-(use-package skk-bayesian
-  :disabled t
-  :init
-  (add-to-list 'load-path "~/.emacs.d/elisp/usr/"))
 
 (use-package htmlize
   :ensure t)
