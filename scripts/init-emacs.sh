ea771383806a9dff9949e77f6df9fcc4db31da6b
Hiroki Furusho <furusityofu@gmail.com>
2020-02-29 12:51:59 +0900

    init-emach.shを書き換え

--- scripts/init-emacs.sh
+++ scripts/init-emacs.sh
@@ -1,4 +1,7 @@
-ln -s $HOME/git/dotfile/.emacs.d/init.el $HOME/.emacs.d/
-ln -s $HOME/git/dotfile/.emacs.d/conf $HOME/.emacs.d/
-ln -s $HOME/git/dotfile/.emacs.d/snippets $HOME/.emacs.d/
-ln -s $HOME/git/dotfile/.emacs.d/lisp $HOME/.emacs.d/
+#!/bin/sh
+cd `dirname $0`                 # ファイルのある場所をカレントディレクトリにする
+ln -s $PWD/../.emacs.d/init.el $HOME/.emacs.d/
+ln -s $PWD/../.emacs.d/conf $HOME/.emacs.d/
+ln -s $PWD/../.emacs.d/snippets $HOME/.emacs.d/
+ln -s $PWD/../.emacs.d/lisp $HOME/.emacs.d/
+ln -s $PWD/../.emacs.d/straight $HOME/.emacs.d/
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-
 cd `dirname $0`                 # ファイルのある場所をカレントディレクトリにする
-
 ln -s $PWD/../.editorconfig $HOME/
 ln -s $PWD/../.zshrc $HOME/
 touch $HOME/.zshrc.local.zsh
 ln -s $PWD/../.latexmkrc $HOME/
 ln -s $PWD/../.gitconfig $HOME/
+sh ./init-emacs.sh
