7152f9fa60c187ba9fc6d13c5b55469b5d683d71
Hiroki Furusho <furusityofu@gmail.com>
2020-02-29 10:55:57 +0900

    init.shを追加

---
+++ scripts/init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd `dirname $0`                 # ファイルのある場所をカレントディレクトリにする
+
+ln -s $PWD/../.editorconfig $HOME/
+ln -s $PWD/../.zshrc $HOME/
+touch $HOME/.zshrc.local.zsh
+ln -s $PWD/../.latexmkrc $HOME/
