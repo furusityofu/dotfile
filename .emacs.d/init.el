ff39fd4c2f7ced98bdff49fe9e8aef54e10b25ad
Hiroki Furusho <furusityofu@gmail.com>
2020-03-22 21:10:50 +0900

    ewwの検索をhttpsで行う

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -333,8 +333,9 @@
 
 (use-package eww
   :commands (eww)
+  :straight nil
   :config
-  (setq eww-search-prefix "http://www.google.co.jp/search?q=")
+  (setq eww-search-prefix "https://www.google.co.jp/search?q=")
   (defun eww-disable-images ()
     "eww で画像表示させない"
     (interactive)
