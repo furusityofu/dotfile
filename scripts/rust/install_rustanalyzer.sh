16616e4c0f323141ee0dac5a883a704093453f9e
Hiroki Furusho <furusho@imac5k.local>
2020-08-05 18:53:00 +0900

    rust analyzerのインストールスクリプトを追加

---
+++ scripts/rust/install_rustanalyzer.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-mac -o ~/.local/bin/rust-analyzer
+chmod +x ~/.local/bin/rust-analyzer
