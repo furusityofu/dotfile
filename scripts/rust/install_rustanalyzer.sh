f188cb3bb81e1f1157ffe064332cf0e749f11172
Hiroki Furusho <furusho@DESKTOP-ITJID1T.localdomain>
2020-08-14 14:14:06 +0900

    rust-analyzerのダウンロードスクリプトを修正

--- scripts/rust/install_rustanalyzer.sh
+++ scripts/rust/install_rustanalyzer.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-mac -o ~/.local/bin/rust-analyzer
+if [ "$(uname)" == 'Darwin' ]; then
+    OS='mac'
+    echo $OS
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    OS='linux'
+    echo $OS
+else
+    echo "Your platform ($(uname -a)) is not supported."
+    exit 1
+fi
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-$OS -o ~/.local/bin/rust-analyzer
 chmod +x ~/.local/bin/rust-analyzer
