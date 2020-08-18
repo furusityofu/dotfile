f941770a21aae522507c0687178c8c8079110ab5
Hiroki Furusho <furusho@imac5k.local>
2020-08-18 15:52:28 +0900

    セットアップ用スクリプトの修正

--- scripts/init-emacs.sh
+++ scripts/init-emacs.sh
@@ -2,7 +2,7 @@
 cd `dirname $0`                 # ファイルのある場所をカレントディレクトリにする
 EMACS_CONFIG_DIR=$HOME/.config/emacs/
 ln -s $PWD/../.emacs.d/init.el $EMACS_CONFIG_DIR
-ln -s $PWD/../.emacs.d/conf $EMACS_CONFIG_DIR
+ln -s $PWD/../.emacs.d/early-init.el $EMACS_CONFIG_DIR
 ln -s $PWD/../.emacs.d/snippets $EMACS_CONFIG_DIR
 ln -s $PWD/../.emacs.d/lisp $EMACS_CONFIG_DIR
 ln -s $PWD/../.emacs.d/straight $EMACS_CONFIG_DIR
