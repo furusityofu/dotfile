e67c7914698a46482a54be4ace94eb42d6de3650
Hiroki Furusho <furusityofu@gmali.com>
2020-08-27 13:00:22 +0900

    emacsの初期ディレクトリを~/.emacs.d/に変更

--- scripts/init-emacs.sh
+++ scripts/init-emacs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd `dirname $0`                 # ファイルのある場所をカレントディレクトリにする
-EMACS_CONFIG_DIR=$HOME/.config/emacs/
+EMACS_CONFIG_DIR=$HOME/.emacs.d/
 ln -s $PWD/../.emacs.d/init.el $EMACS_CONFIG_DIR
 ln -s $PWD/../.emacs.d/early-init.el $EMACS_CONFIG_DIR
 ln -s $PWD/../.emacs.d/snippets $EMACS_CONFIG_DIR
