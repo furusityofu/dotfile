32344280b97d97871240e89449368e6c4fa78c01
Hiroki Furusho <furusho@fmbp.local>
2020-08-29 01:19:37 +0900

    latexmkrcの場所を変更

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,4 +8,5 @@ mkdir -p $HOME/.config/git/
 ln -s $PWD/../.gitconfig $HOME/.config/git/config
 touch $HOME/.config/git/config.local
 ln -s $PWD/../.config/bat/ $HOME/.config/
+ln -s $PWD/../.config/latexmk/ $HOME/.config/
 sh ./init-emacs.sh
