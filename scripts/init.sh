1f78f3a44a85e39bebf1b0f3b364f07f4bb2f7db
Hiroki Furusho <furusityofu@gmail.com>
2020-10-25 20:34:27 +0900

    pinentryのパスを追加

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,6 +2,8 @@
 cd `dirname $0`                 # ファイルのある場所をカレントディレクトリにする
 ln -s $PWD/../.editorconfig $HOME/
 ln -s $PWD/../.zshrc $HOME/
+ln -s $PWD/../.gnupg/gpg.conf $HOME/.gnupg/
+ln -s $PWD/../.gnupg/gpg-agent.conf $HOME/.gnupg/
 touch $HOME/.zshrc.local.zsh
 mkdir -p $HOME/.config/
 ln -s $PWD/../.config/git/ $HOME/.config/
