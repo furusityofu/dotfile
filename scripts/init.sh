4bf942608fd1d8688a4c1ac37f6b028a07cb302e
Hiroki Furusho <furusityofu@gmail.com>
2020-11-03 12:32:46 +0900

    gitのローカル設定ファイルをgitから除外

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,6 +7,12 @@ ln -s $PWD/../.gnupg/gpg-agent.conf $HOME/.gnupg/
 touch $HOME/.zshrc.local.zsh
 mkdir -p $HOME/.config/
 ln -s $PWD/../.config/git/ $HOME/.config/
+if [ ! -f $HOME/.config/git/config.local ];then
+    echo '[user]
+	name = ""
+	email = ""
+' > $HOME/.config/git/config.local
+fi
 ln -s $PWD/../.config/bat/ $HOME/.config/
 ln -s $PWD/../.config/latexmk/ $HOME/.config/
 ln -s $PWD/../.config/tig/ $HOME/.config/
