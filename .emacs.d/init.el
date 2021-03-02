243351705c1572c9b83d0cf281ab68ae95a94caf
Hiroki Furusho <furusityofu@gmail.com>
2021-03-02 20:39:41 +0900

    org-eldocの修正

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -893,18 +893,16 @@
 
 ;; Org Mode LaTeX Export
 
-(use-package ox-eldoc
-  :straight nil
-  :defer t
+(leaf org-eldoc
+  :after org
+  :require t
+  :hook (org-mode-hook . eldoc-mode)
   :config
   (defadvice org-eldoc-documentation-function (around add-field-info activate)
     (or
      (ignore-errors (and (not (org-at-table-hline-p))
                          (org-table-field-info nil)))
      ad-do-it))
-
-  (add-hook 'org-mode-hook 'eldoc-mode)
-
   (eldoc-add-command-completions
    "org-table-next-" "org-table-previous" "org-cycle"))
 
