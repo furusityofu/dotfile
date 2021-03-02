5ea96558c1c3347017052b1ad9a92eda9de736ed
Hiroki Furusho <furusityofu@gmail.com>
2021-03-02 19:40:02 +0900

    起動時間を短縮する設定を追加

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,3 +1,18 @@
+(defconst my:saved-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq file-name-handler-alist my:saved-file-name-handler-alist)))
+
+
+(defconst my:default-gc-cons-threshold (* 1024 1024 16))
+(setq gc-cons-threshold most-positive-fixnum)
+;; Run GC every 60 seconds if emacs is idle.
+(run-with-idle-timer 60.0 t #'garbage-collect)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold my:default-gc-cons-threshold)))
+
 (setq system-time-locale "C")
 (setenv "LANG" "en_US.UTF-8")
 (setenv "LC_ALL" "en_US.UTF-8")
