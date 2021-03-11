842d128454168ae7416dd737621b483bdcc7e0af
Hiroki Furusho <furusityofu@gmail.com>
2021-03-11 11:55:28 +0900

    org-protocolを有効化

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -874,6 +874,10 @@
              :base-extension "jpg\\|png\\|pdf"
              :publishing-function org-publish-attachment
              :recursive t))))
+  (leaf org-protocol
+    :require t
+    :config
+    (add-to-list 'org-modules 'org-protocol))
   (leaf org-mu4e
     :disabled t
     :straight t
