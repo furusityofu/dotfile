0e7c3a9758d49d332e8b3973b7d6d37d6c3d5f3d
Hiroki Furusho <furusityofu@gmail.com>
2021-03-30 01:53:23 +0900

    ddskkのsticky shiftのキーを設定

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -240,6 +240,7 @@
    (skk-isearch-start-mode . 'latin); isearch で skk の初期状態
    (skk-user-directory . ,(format "%sddskk/" user-emacs-directory))
    (skk-henkan-strict-okuri-precedence . t)
+   (skk-sticky-key . '(117 104))
    )
   :init
   (push (lambda ()
